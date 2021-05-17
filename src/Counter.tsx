dac1fe1f8822b921159d209fe146590007a19976
”Anastasiia <nastja.gusarenko@gmail.com>
2021-05-17 13:41:12 +0300

    fixed CounterTable

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -15,15 +15,31 @@ type CounterPropsType = {
 export const Counter = (props: CounterPropsType) => {
 
 
-    const onClickIncDisabled = props.numb >= props.maxValue;
-    const onClickResetDisabled = props.numb <= props.startValue ;
+    const onClickIncDisabled = props.numb >= props.maxValue || props.settingsActive;
+    const onClickResetDisabled = props.numb <= props.startValue || props.startValue < 0;
 
+    const showMessage = () => {
+        if (props.settingsActive && props.startValue >= 0 && props.startValue < props.maxValue) {
+            return <div className={styles.text}>
+                enter values and press set</div>
+        } else if (props.startValue < 0 || props.startValue >= props.maxValue) {
+            return <div className={styles.error}>
+                incorrect value</div>
+        } else {
+            return <div className={props.numb >= props.maxValue ? styles.maxNumb : ""}>{props.numb}</div>
+        }
+
+    }
 
     return (
+
         <div className={styles.wrapper}>
             <div className={styles.number}>
-                {props.settingsActive ? <div className={props.startValue < props.maxValue ? styles.text : ""}>enter values and press set</div> :
-                    <div className={props.numb >= props.maxValue ? styles.maxNumb : ""}>{props.numb}</div>}
+                {showMessage()}
+                {/*{props.settingsActive ?*/}
+                {/*    <div className={props.startValue >= 0 && props.startValue < props.maxValue ? styles.text : ""}>*/}
+                {/*        enter values and press set</div> :*/}
+                {/*    <div className={props.numb >= props.maxValue ? styles.maxNumb : ""}>{props.numb}</div>}*/}
             </div>
             <div className={styles.buttonForm}>
                 <Button
@@ -36,5 +52,6 @@ export const Counter = (props: CounterPropsType) => {
                     disabled={onClickResetDisabled}/>
             </div>
         </div>
+
     )
 }
